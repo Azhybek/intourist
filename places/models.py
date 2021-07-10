3f653fabab24f295e992edcf852993c90e161534
Azhybek Nurlanov <https://github.com/Azhybek/exem_python.git>
2021-07-10 20:24:21 +0600

    add places model, views, urls

--- places/models.py
+++ places/models.py
@@ -1,3 +1,21 @@
+from functools import update_wrapper
 from django.db import models
+from django.db.models.fields import TextField
 
-# Create your models here.
+
+class Place(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    views_count = models.IntegerField(default=0)
+    is_publicated = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'место'
+        verbose_name_plural = 'Места'
+        ordering = ['name']
