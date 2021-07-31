4271328b6975e9f18dd5289d3d622e62668bc8d9
Azhybek Nurlanov <https://github.com/Azhybek/exem_python.git>
2021-07-31 14:16:44 +0600

    add admin panels

--- places/admin.py
+++ places/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from .models import Feedback, Place
 
 admin.site.register(Place)
-admin.site.register(Feedback)
+
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ['text', 'place', 'user', 'checked']
+    list_editable = ['checked']
+    list_filter = ['checked']
+    search_fields = ['text', 'place__name', 'place_location', 'place__description']
+
+    fields = ['user', 'place', 'text']
+    readonly_fields = ['place', 'text']
+
+admin.site.register(Feedback, FeedbackAdmin)
