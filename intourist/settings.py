920d82cb12b952c897112de0ac7709c695917bf8
Azhybek Nurlanov <https://github.com/Azhybek/exem_python.git>
2021-09-19 22:28:33 +0600

    added place img

--- intourist/settings.py
+++ intourist/settings.py
@@ -83,6 +83,17 @@ DATABASES = {
     }
 }
 
+# база данных postgresql
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'intourist_db',
+#         'USER': 'postgres',
+#         'PASSWORD': "postgres",
+#         'HOST': '127.0.0.1',
+#         'PORT': "5432",
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
